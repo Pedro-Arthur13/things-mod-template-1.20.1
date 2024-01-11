b7eb99fb612a4f0744baf73cd150b50de8c92879
Pedro Arthur <parthurmalbuquerque@gmail.com>
2024-01-11 08:40:15 -0300

    Initial commit

---
+++ src/main/java/com/alucard/thingsmod/ThingsModClient.java
@@ -0,0 +1,10 @@
+package com.alucard.thingsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThingsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/alucard/thingsmod/ThingsModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.alucard.thingsmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThingsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
